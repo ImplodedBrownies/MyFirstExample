e67666f486a45c1bbb996896340d292c0a350b3b
ImplodedBrownies <rubalbajwa05@gmail.com>
2026-02-11 12:23:21 -0800

    error on line 28

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main()
     //std::cout << "Division: " << x / y << std::endl;
     std::cout << "Remainder: " << x % y << std::endl;
     std::cout << "Square Root: " << sqrt(x) << std::endl;
-    std::cout << "Square: " << pow(x, y) << std::endl;
+    std::cout << "Square: " << pow(x, y) << std::endldfdf
 
     return 0;
 }
